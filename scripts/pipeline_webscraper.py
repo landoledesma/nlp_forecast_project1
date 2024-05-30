b03eec97ea09b24f1d129e271474135931973ef4
lando <lando_ledesma@outlook.com>
2024-05-30 10:24:44 -0600

    ensure data/ exist

--- scripts/pipeline_webscraper.py
+++ scripts/pipeline_webscraper.py
@@ -2,6 +2,15 @@ import os
 import subprocess
 import time
 
+def ensure_data_directory_exists():
+    # Verifica si el directorio 'data' existe, si no, lo crea.
+    data_directory = os.path.join(os.getcwd(), 'data')
+    if not os.path.exists(data_directory):
+        os.makedirs(data_directory)
+        print(f"Directorio {data_directory} creado.")
+    else:
+        print(f"Directorio {data_directory} ya existe.")
+        
 def build_and_run_docker_container():
     # Levanta el contenedor Docker usando docker-compose
     subprocess.run(["docker-compose", "up", "--build"])
@@ -19,7 +28,7 @@ def clean_files():
     print("Archivos basura eliminados.")
 
 if __name__ == "__main__":
-    
+    ensure_data_directory_exists()
     build_and_run_docker_container()
     remove_docker_container()
     clean_files()
